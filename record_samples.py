084d74fed4edca83f31acb124b3a989781b26720
Martin Smith <msmith@bitmonster.org>
2025-04-01 23:46:36 -0500

    Test program using AudioDevice API

---
+++ record_samples.py
@@ -0,0 +1,65 @@
+import logging
+import numpy as np
+import pyaudio
+import silero_vad
+import time
+import torch
+
+from audio.AudioDevice import AudioDevice
+from audio.utils import AudioBuffer, AudioData, save_recording, save_recording_mp3
+from utils.arrays import abbreviate_data
+from utils.debug import TimeThis
+
+logger = logging.getLogger(__name__)
+#logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - '%(message)s'")
+
+AUDIO_FORMAT = pyaudio.paInt16
+SILERO_VAD_AUDIO_CHUNK = 512
+    
+def main():
+    # Initialize the AudioDevice
+    device = AudioDevice()
+
+    # Initialize the Silero VAD
+    vad_model = silero_vad.load_silero_vad()
+
+    # Listen to the microphone
+    speech_buffer = AudioBuffer(AUDIO_FORMAT)
+    is_recording = False
+    speech_pause_duration = 0.5
+    pause_duration = 0
+
+    while True:
+        read_size = SILERO_VAD_AUDIO_CHUNK
+        chunk = device.read(read_size)
+
+        # Convert the chunk to a tensor
+        chunk_tensor = torch.tensor(chunk, dtype=torch.int16).float()
+        chunk_tensor /= torch.iinfo(torch.int16).max
+
+        vad_score = vad_model(chunk_tensor, 16000).item()
+        is_speech = vad_score > 0.5
+
+        if is_speech:
+            is_recording = True
+            pause_duration = 0
+        else:
+            pause_duration += read_size / 16000
+
+        if is_recording:
+            speech_buffer.append(chunk)
+
+            if pause_duration >= speech_pause_duration:
+                save_recording(speech_buffer, "speech.wav")
+                save_recording_mp3(speech_buffer, "speech.mp3")
+                print("Recording saved.")
+                # print("Playing back recording...")
+                # recording = AudioData(speech_buffer.get_frames(), AUDIO_FORMAT)
+                # device.play(recording)
+                # time.sleep(recording.duration() + 0.5)
+                speech_buffer.clear()
+                device.reset_microphone()
+                is_recording = False
+
+if __name__ == "__main__":
+    main()
