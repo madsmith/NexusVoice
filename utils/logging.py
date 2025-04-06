d3d505ac0a974ff946d87a90ccdf9b9e7dafa4ee
Martin Smith <msmith@bitmonster.org>
2025-04-06 18:33:47 -0500

    Customize logging with trace level

---
+++ utils/logging.py
@@ -0,0 +1,43 @@
+import logging
+import traceback
+
+LOG_TRACE_LEVEL = logging.DEBUG // 2
+logging.addLevelName(LOG_TRACE_LEVEL, "TRACE")
+
+def trace(self, message, *args, **kwargs):
+    if self.isEnabledFor(LOG_TRACE_LEVEL):
+        self._log(LOG_TRACE_LEVEL, message, args, **kwargs)
+
+logging.Logger.trace = trace
+
+# Function to get a pre-configured logger
+def get_logger(name: str = __name__) -> logging.Logger:
+    logger = logging.getLogger(name)
+    # if not logger.hasHandlers():
+    #     # Configure the logger if it doesn't already have handlers
+    #     handler = logging.StreamHandler()
+    #     formatter = logging.Formatter("[%(levelname)s] %(name)s: %(message)s")
+    #     handler.setFormatter(formatter)
+    #     logger.addHandler(handler)
+    #     logger.setLevel(logging.DEBUG)  # Default level
+    return logger
+
+class StackTraceFormatter(logging.Formatter):
+    def format(self, record):
+        # Format the original log message
+        message = super().format(record)
+        # Append the stack trace if available
+        if record.exc_info:
+            message += "\n" + "".join(traceback.format_exception(*record.exc_info))
+        else:
+            # Add the current stack trace for non-exception logs
+            stack = traceback.format_stack()
+            message += "\n" + "".join(stack[:-1])  # Exclude the current log call
+        return message
+
+# # Configure the logger
+# log_format = "[%(asctime)s] [%(levelname)s] - %(name)s (%(pathname)s:%(lineno)d): %(message)s"
+# handler = logging.StreamHandler()
+# handler.setFormatter(StackTraceFormatter(log_format))
+# logging.getLogger().addHandler(handler)
+# logging.getLogger().setLevel(logging.DEBUG)
