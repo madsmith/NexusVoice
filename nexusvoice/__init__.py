1ada0472900a71c263b44602bb29857aa8f9d2eb
Martin Smith <msmith@bitmonster.org>
2025-04-23 17:10:06 -0500

    Environment cleanup, Dev/Prod modes

--- nexusvoice/__init__.py
+++ nexusvoice/__init__.py
@@ -1,4 +1,13 @@
+import sys
+import os
 from nexusvoice.bootstrap import setup_environment, initialize_openwakeword
 
-setup_environment()
+# Determine if the executing script is in the 'scripts' folder
+script_path = os.path.abspath(sys.argv[0])
+if os.path.sep + "scripts" + os.path.sep in script_path or script_path.endswith(os.path.sep + "scripts"):
+    environment_mode = 'DEV'
+else:
+    environment_mode = 'PROD'
+
+setup_environment(environment_mode)
 initialize_openwakeword()
--- nexusvoice/bootstrap.py
+++ nexusvoice/bootstrap.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 import warnings
 
 from nexusvoice.utils.logging import get_logger
@@ -6,13 +7,13 @@ logger = get_logger(__name__)
 
 import openwakeword
 
-
-def setup_environment():
+def setup_environment(environment_mode: Literal['DEV', 'PROD']):
     os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-    suppress_warnings()
+    suppress_warnings(environment_mode)
 
-def suppress_warnings():
+def suppress_warnings(environment_mode: Literal['DEV', 'PROD']):
     warnings.filterwarnings(
         "ignore", message=".*will fall back to run on the CPU.*", category=UserWarning
     )
@@ -22,16 +23,21 @@ def suppress_warnings():
     warnings.filterwarnings(
         "ignore", message="`torch.nn.utils.weight_norm` is deprecated in favor", category=FutureWarning
     )
+    warnings.filterwarnings(
+        "ignore", message="`use_mps_device` is deprecated and will be removed", category=UserWarning
+    )
 
-    from transformers.utils import logging
-
-    logging.set_verbosity_error()
-    logging.disable_progress_bar()
+    if environment_mode == 'PROD':
+        from transformers.utils import logging
+        logging.set_verbosity_error()
+        logging.disable_progress_bar()
 
 def initialize_openwakeword():
     model_paths = openwakeword.get_pretrained_model_paths()
 
-    all_paths_exist = all(os.path.exists(model_path) for model_path in model_paths)
-    if not all_paths_exist:
-        logger.info("Downloading OpenWakeWord models...")
-        openwakeword.utils.download_models()
+    if model_paths:
+        all_paths_exist = all(os.path.exists(model_path) for model_path in model_paths)
+        if not all_paths_exist:
+            from openwakeword.utils import download_models
+            logger.info("Downloading OpenWakeWord models...")
+            download_models()
