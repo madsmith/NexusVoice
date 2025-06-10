bcf8f71b8f9c75cb44d1dd06f0429d171dcbebed
Martin Smith <msmith@bitmonster.org>
2025-06-09 19:59:06 -0500

    logfire wrapper for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import logfire
+import pytest
+
+service_name = "pytest"
+
+logfire.configure(
+    console=False,
+    show_summary=False,
+    send_to_logfire=True,
+    service_name=service_name,
+)
+
+def pytest_collection(session: pytest.Session):
+    session.stash["session_span"] = logfire.span("test session")
+
+
+def pytest_itemcollected(item: pytest.Item):
+    if "parent_span" not in item.parent.stash:
+        item.parent.stash["parent_span"] = logfire.span(item.parent.name)
+
+    item.stash["item_span"] = logfire.span(item.name)
+
+
+@pytest.hookimpl(wrapper=True)
+def pytest_runtest_protocol(item: pytest.Item):
+    with item.session.stash["session_span"]:
+        with item.parent.stash["parent_span"]:
+            with item.stash["item_span"]:
+                yield
+
+
+def pytest_exception_interact(node: pytest.Item, call: pytest.CallInfo):
+    logfire.exception(str(call.excinfo.value), _exc_info=call.excinfo.value)
