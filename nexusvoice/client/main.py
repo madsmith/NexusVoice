72901f9d99902bfd904d38f792d8d8b279c6910f
Martin Smith <msmith@bitmonster.org>
2025-06-10 14:28:33 -0500

    Run entire app under logfire span

--- nexusvoice/client/main.py
+++ nexusvoice/client/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logfire
 import logging
 import argparse
 from nexusvoice.utils.debug import reset_logging
@@ -13,32 +14,33 @@ from nexusvoice.core.config import load_config
 async def run_client():
     client = None
     try:
-        parser = argparse.ArgumentParser(description="NexusVoice Online Client")
-        parser.add_argument('-c', '--cmd', nargs=argparse.REMAINDER, help='Send a command prompt to the client')
-        args = parser.parse_args()
+        with logfire.span("NexusVoiceClient"):
+            parser = argparse.ArgumentParser(description="NexusVoice Online Client")
+            parser.add_argument('-c', '--cmd', nargs=argparse.REMAINDER, help='Send a command prompt to the client')
+            args = parser.parse_args()
 
-        log_format = "[{levelname}]\t{threadName}\t{name}\t{message}"
-        log_level = logging.DEBUG
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter(log_format, style="{"))
-        logging.basicConfig(level=log_level, style="{", format=log_format, handlers=[handler])
+            log_format = "[{levelname}]\t{threadName}\t{name}\t{message}"
+            log_level = logging.DEBUG
+            handler = logging.StreamHandler()
+            handler.setFormatter(logging.Formatter(log_format, style="{"))
+            logging.basicConfig(level=log_level, style="{", format=log_format, handlers=[handler])
 
-        logger.debug("Loading config")
-        config = load_config()
+            logger.debug("Loading config")
+            config = load_config()
 
-        reset_logging(config.get("logging.suppress"))
- 
-        logger.debug("Creating NexusVoiceClient")
-        client = NexusVoiceClient("local", config)
-        # In the future, NexusVoiceClient should support async start/stop
-        client_running = client.run()
+            reset_logging(config.get("logging.suppress"))
+    
+            logger.debug("Creating NexusVoiceClient")
+            client = NexusVoiceClient("local", config)
+            # In the future, NexusVoiceClient should support async start/stop
+            client_running = client.run()
 
-        if args.cmd:
-            prompt = " ".join(args.cmd).strip()
-            if prompt:
-                client.add_command(NexusVoiceClient.CommandProcessText(prompt))
+            if args.cmd:
+                prompt = " ".join(args.cmd).strip()
+                if prompt:
+                    client.add_command(NexusVoiceClient.CommandProcessText(prompt))
 
-        await client_running
+            await client_running
     except KeyboardInterrupt:
         logger.warning(f"Exiting due to KeyboardInterrupt - run_client")
         if client:
