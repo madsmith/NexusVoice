3d1a86dc01d4faf5563da88a689b479aafdd088f
Martin Smith <msmith@bitmonster.org>
2025-07-20 16:08:39 -0500

    Program startup debug logging switch

--- nexusvoice/client/main.py
+++ nexusvoice/client/main.py
@@ -1,38 +1,46 @@
+import argparse
 import asyncio
 import logfire
 import logging
-import argparse
-from nexusvoice.utils.debug import reset_logging
 from omegaconf import OmegaConf
 
+from nexusvoice.bootstrap import get_logfire
 from nexusvoice.utils.logging import get_logger
 logger = get_logger(__name__)
 
+from nexusvoice.utils.debug import reset_logging
 from nexusvoice.client.NexusClient import NexusVoiceClient
 from nexusvoice.client.commands import CommandProcessText
 from nexusvoice.core.config import load_config
 
 async def run_client():
-    client = None
+    client: NexusVoiceClient | None = None
     try:
         with logfire.span("NexusVoiceClient"):
             parser = argparse.ArgumentParser(description="NexusVoice Online Client")
             parser.add_argument('-c', '--cmd', nargs=argparse.REMAINDER, help='Send a command prompt to the client')
+            parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose logging')
             args = parser.parse_args()
-
-            log_format = "[{levelname}]\t{threadName}\t{name}\t{message}"
-            log_level = logging.DEBUG
-            handler = logging.StreamHandler()
-            handler.setFormatter(logging.Formatter(log_format, style="{"))
-            logging.basicConfig(level=log_level, style="{", format=log_format, handlers=[handler])
 
             logger.debug("Loading config")
             config = load_config()
 
             reset_logging(config.get("logging.suppress"))
-    
-            logger.debug("Creating NexusVoiceClient")
+
+            if args.verbose:
+                logging.getLogger().setLevel(logging.TRACE) # type: ignore
+                
+                # TODO: redo bootstrap process so we don't have to dig deep into 
+                # framework internals to reconfigure
+                instance = get_logfire()
+                assert instance is not None
+                assert isinstance(instance.config.console, logfire.ConsoleOptions)
+                instance.config.console.min_log_level = "trace"
+                instance.config._initialized = False
+                instance.config.initialize()
+            
             client = NexusVoiceClient("local", config)
+            
             # In the future, NexusVoiceClient should support async start/stop
             run_coro = client.run()
 
@@ -44,7 +52,7 @@ async def run_client():
             await run_coro
     except KeyboardInterrupt:
         logger.warning(f"Exiting due to KeyboardInterrupt - run_client")
-        if client:
+        if client is not None:
             await client.stop()
     except Exception as e:
         logger.error(e)
